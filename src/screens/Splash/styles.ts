543924ea62c94b6df61fe7c3b2ff63699b110910
athospugliesedev <athospugliesedev@gmail.com>
2023-04-20 10:51:29 -0300

    initial commit

---
+++ src/screens/Splash/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: blue;
+`;
+
+export const ImageBg = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  `;
+
+export const Title = styled.Text`
+  color: pink;
+`;
